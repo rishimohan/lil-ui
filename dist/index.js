cae2eac50bbbca5b67188c4d2025a5f26f59b604
jawblia <juliabell021@gmail.com>
2021-04-01 08:46:17 -0400

    repo sent to npm

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Badge", {
+  enumerable: true,
+  get: function get() {
+    return _Badge.default;
+  }
+});
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function get() {
+    return _Button.default;
+  }
+});
+
+var _Badge = _interopRequireDefault(require("./components/Badge"));
+
+var _Button = _interopRequireDefault(require("./components/Button"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
