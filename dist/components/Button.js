ab22c28153d72959ac78888e8bfe0662f8185090
Rishi <iamrishi.ms@gmail.com>
2022-05-26 01:07:38 +0530

    fix: button component style

--- dist/components/Button.js
+++ dist/components/Button.js
@@ -7,6 +7,8 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+require("./styles/button.scss");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const Button = _ref => {
